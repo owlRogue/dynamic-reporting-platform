1da40c20b48e3183c99d858888f621f8e9bd75c6
Koltyn Palmer <palmer@ring.com>
2018-07-24 16:07:52 -0600

    protect sheets

--- public/credentials/Sheets.js
+++ public/credentials/Sheets.js
@@ -8,24 +8,24 @@ var sheet_title;
 
 var sheet = [
   (defaultSheet = {
-      sheet_ID: "1GVluuYF9hAgCu5fH9nUqbrQsb-j-r8kLaRFSKE0thi8",
-      sheet_range: ['Legend - Source!A1:F37'],
-      sheet_title: 'Legend'
+      sheet_ID: "",
+      sheet_range: [''],
+      sheet_title: ''
   }),
   (sheet1 = {
-      sheet_ID: '1M9B-5Y5y5acwdulaIZ9-DKv7HOhJg9ur79rqB4FfctE',
-      sheet_range: ['A1:M300'],
-      sheet_title: 'Team MtD'
+      sheet_ID: '',
+      sheet_range: [''],
+      sheet_title: ''
   }),
   (sheet2 = {
-      sheet_ID: '1SB_o8MuffgbFURB_YxxzqXpaFLrYsvAEUWovrSDjxFE',
-      sheet_range: ['A1:R200'],
-      sheet_title: 'Agent MtD'
+      sheet_ID: '',
+      sheet_range: [''],
+      sheet_title: ''
   }),
   (sheet3 = {
       sheet_ID: '1HK8EjM8FiBzu2gMaDlsi_Fd92fjx4rG7X1VTW48pP6Q',
       sheet_range: ['A:E'],
-      sheet_title: 'Kolt is shared'
+      sheet_title: 'KoltSharedOn'
   }),
   (sheet5 = {
       sheet_ID: '1l1GyTc65N-afxhIJ-9O7eogKYdOIUvZQw1m1D9IWoyQ',
@@ -40,6 +40,6 @@ var sheet = [
   (sheet7 = {
       sheet_ID: '1hV5e_pg2rvRZJJw7-eeEqEwnYeXuGeI-w10y4ydxJVE',
       sheet_range: ['A:E'],
-      sheet_title: 'Kolt owns'
+      sheet_title: 'KoltOwns'
   })
 ];
