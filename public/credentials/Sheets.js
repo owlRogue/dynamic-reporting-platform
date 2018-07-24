d3dfa8bc432647a3f853d119fb6260aa93e834fc
Koltyn Palmer <palmer@ring.com>
2018-07-24 15:47:30 -0600

    unhiding sheets for heroku deploy

---
+++ public/credentials/Sheets.js
@@ -0,0 +1,45 @@
+// INSTRUCTIONS :
+// Retitle to Sheets.js
+// INSTRUCTIONS: Retitle to Sheets.js
+
+var sheet_ID;
+var sheet_range;
+var sheet_title;
+
+var sheet = [
+  (defaultSheet = {
+      sheet_ID: "1GVluuYF9hAgCu5fH9nUqbrQsb-j-r8kLaRFSKE0thi8",
+      sheet_range: ['Legend - Source!A1:F37'],
+      sheet_title: 'Legend'
+  }),
+  (sheet1 = {
+      sheet_ID: '1M9B-5Y5y5acwdulaIZ9-DKv7HOhJg9ur79rqB4FfctE',
+      sheet_range: ['A1:M300'],
+      sheet_title: 'Team MtD'
+  }),
+  (sheet2 = {
+      sheet_ID: '1SB_o8MuffgbFURB_YxxzqXpaFLrYsvAEUWovrSDjxFE',
+      sheet_range: ['A1:R200'],
+      sheet_title: 'Agent MtD'
+  }),
+  (sheet3 = {
+      sheet_ID: '1HK8EjM8FiBzu2gMaDlsi_Fd92fjx4rG7X1VTW48pP6Q',
+      sheet_range: ['A:E'],
+      sheet_title: 'Kolt is shared'
+  }),
+  (sheet5 = {
+      sheet_ID: '1l1GyTc65N-afxhIJ-9O7eogKYdOIUvZQw1m1D9IWoyQ',
+      sheet_range: ['A1:Q100'],
+      sheet_title: 'DummyMTDScoreCard'
+  }),
+  (sheet6 = {
+      sheet_ID: '1KbuidBzpeH_i6X-1QoXgxcyi2PoldMyiBtqvRX_OuAQ',
+      sheet_range: ['A1:Z1000'],
+      sheet_title: 'StressTestingBigData'
+  }),
+  (sheet7 = {
+      sheet_ID: '1hV5e_pg2rvRZJJw7-eeEqEwnYeXuGeI-w10y4ydxJVE',
+      sheet_range: ['A:E'],
+      sheet_title: 'Kolt owns'
+  })
+];
