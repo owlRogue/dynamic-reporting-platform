347bad4de94c1ec1c74a08890a2979de93473635
Koltyn Palmer <palmer@ring.com>
2019-02-03 19:12:22 -0700

    up to date 2019

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// To https://github.com/owlRogue/dynamic-reporting-platform.git
+
 // Dependencies
 // =============================================================
 var express = require("express");
@@ -42,3 +44,37 @@ app.listen(port, function() {
 // app.get("/api/sheets", function(req, res) {
 //   return res.json(characters);
 // });
+
+
+// Displays a single character, or returns false
+// app.get("/api/characters/:character", function(req, res) {
+//   var chosen = req.params.character;
+
+//   console.log(chosen);
+
+//   for (var i = 0; i < characters.length; i++) {
+//     if (chosen === characters[i].routeName) {
+//       return res.json(characters[i]);
+//     }
+//   }
+
+//   return res.json(false);
+// });
+
+// Create New Characters - takes in JSON input
+// app.post("/api/characters", function(req, res) {
+//   // req.body hosts is equal to the JSON post sent from the user
+//   // This works because of our body-parser middleware
+//   var newcharacter = req.body;
+
+//   // Using a RegEx Pattern to remove spaces from newCharacter
+//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//   newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+//   console.log(newcharacter);
+
+//   characters.push(newcharacter);
+
+//   res.json(newcharacter);
+// });
+
